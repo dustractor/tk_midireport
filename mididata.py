9501938c68960e7c408683d24e159e58925a2b6f
Shams Kitz <dustractor@gmail.com>
2023-10-21 01:38:17 -0500

    Update mididata.py

--- mididata.py
+++ mididata.py
@@ -5,8 +5,8 @@ from itertools import cycle
 
 home = pathlib.Path.home()
 here = pathlib.Path(__file__).parent
-_TESTING = True
-# _TESTING = False
+# _TESTING = True
+_TESTING = False
 _DBFILE =  here / "midis.db"
 _DEFAULTROOTDIR = home /"Documents"/"Image-Line"/"FL Studio"/"Presets"/"Scores" 
 _TESTINGROOT = home / "Desktop" / "testmidis"
@@ -30,6 +30,8 @@ note_d = {a:b for (a,b) in zip(
     cycle(["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]))}
 
 
+_ANYKEY = "*ANY*"
+_NOTECOUNTS = ["0","1-10","11-100","101-1000",">1000"]
 class MidiLibrarian(sqlite3.Connection):
     ddl = """
     create table if not exists midis (
@@ -96,8 +98,11 @@ class MidiLibrarian(sqlite3.Connection):
 
     def distinct_keys(self):
         return list(self.cu.execute("select distinct keys from midis"))
-    def data_with_key(self,key):
-        return list(self.cu.execute("select name from midis where keys=?",(key,)))
+    def datatree_view(self,key):
+        if key == _ANYKEY:
+            return list(self.execute("select id,path,name from midis"))
+        else:
+            return list(self.execute("select id,path,name from midis where keys=?",(key,)))
 
 
 class MidiLibrary:
@@ -114,13 +119,20 @@ class MidiLibrary:
 
 class App(tk.Tk):
     def update_ui(self):
-        self.keys.set(db.cx.distinct_keys())
+        self.keys.set([_ANYKEY]+list(db.cx.distinct_keys()))
         active_key = self.active_key.get()
         if active_key:
-            print("active_key:",active_key)
-            self.data.set(db.cx.data_with_key(active_key))
+            self.fill_datatree(active_key)
         else:
-            print("no active key")
+            self.clear_datatree()
+
+    def clear_datatree(self):
+        self.datatree.delete(*self.datatree.get_children())
+
+    def fill_datatree(self,key):
+        self.clear_datatree()
+        for t in db.cx.datatree_view(key):
+            self.datatree.insert("","end",text=t[2],values=(t[0],t[1]))
 
     def keylist_select(self,event):
         index = self.keylist.curselection()
@@ -129,11 +141,17 @@ class App(tk.Tk):
             self.active_key.set(item)
         self.update_ui()
 
-    def datalist_select(self,event):
-        index = self.datalist.curselection()
+    def datatree_select(self,event):
+        selection = self.datatree.selection()
+        t = self.datatree.item(selection,"values")
+        path = t[1]
+        self.active_data.set(path)
+
+    def notectlist_select(self,event):
+        index = self.notectlist.curselection()
         if len(index):
-            item = self.datalist.get(index[0])
-            self.active_data.set(item)
+            item = self.notectlist.get(index[0])
+            self.active_notecount.set(item)
         self.update_ui()
 
     def data_action(self):
@@ -141,20 +159,27 @@ class App(tk.Tk):
         print("data:",data)
         subprocess.run("explorer /select,\"{}\"".format(data),shell=True)
 
-
     def __init__(self):
         super().__init__()
         self.keys = tk.StringVar()
-        self.data = tk.StringVar()
         self.active_key = tk.StringVar()
         self.active_data = tk.StringVar()
+        self.active_notecount = tk.StringVar()
+        self.notecounts = tk.StringVar()
+        self.notecounts.set(_NOTECOUNTS)
+        
 
         self.activekeylabel = tk.Label(self,textvariable=self.active_key)
         self.activedatalabel = tk.Button(self,command=self.data_action,textvariable=self.active_data)
+        self.activenotectlabel = tk.Label(self,textvariable=self.active_notecount)
         self.mainframe = ttk.LabelFrame(self,text="Midi File Data")
         self.mainframe.pack(fill="both",expand=True)
-        self.keyframe = ttk.LabelFrame(self.mainframe,labelwidget=self.activekeylabel)
-        self.keyframe.pack(fill="both",expand=True,side="left")
+        self.filterframe = ttk.Frame(self.mainframe)
+        self.filterframe.pack(fill="both",expand=True,side="left")
+        self.keyframe = ttk.LabelFrame(self.filterframe,labelwidget=self.activekeylabel)
+        self.keyframe.pack(fill="both",expand=True)
+        self.notectframe = ttk.LabelFrame(self.filterframe,labelwidget=self.activenotectlabel)
+        self.notectframe.pack(fill="both",expand=True)
         self.dataframe = ttk.LabelFrame(self.mainframe,labelwidget=self.activedatalabel)
         self.dataframe.pack(fill="both",expand=True,side="right")
         self.keylist = tk.Listbox(self.keyframe,listvariable=self.keys)
@@ -163,14 +188,21 @@ class App(tk.Tk):
         self.keylistscroll.pack(fill="y",side="right",anchor="w")
         self.keylist.configure(yscrollcommand=self.keylistscroll.set)
         self.keylistscroll.configure(command=self.keylist.yview)
-        self.datalist = tk.Listbox(self.dataframe,listvariable=self.data,width=72)
-        self.datalist.pack(fill="both",expand=True,side="left")
-        self.datalistscroll = tk.Scrollbar(self.dataframe)
-        self.datalistscroll.pack(fill="y",side="right",anchor="w")
-        self.datalist.configure(yscrollcommand=self.datalistscroll.set)
-        self.datalistscroll.configure(command=self.datalist.yview)
+        self.notectlist = tk.Listbox(self.notectframe,listvariable=self.notecounts)
+        self.notectlist.pack(fill="both",expand=True,side="left")
+        self.notectscroll = tk.Scrollbar(self.notectframe)
+        self.notectscroll.pack(fill="y",side="right",anchor="w")
+        self.notectlist.configure(yscrollcommand=self.notectscroll.set)
+        self.notectscroll.configure(command=self.notectlist.yview)
+        self.datatree = ttk.Treeview(self.dataframe)
+        self.datatree.pack(fill="both",expand=True,side="left")
+        self.datatreescroll = tk.Scrollbar(self.dataframe)
+        self.datatreescroll.pack(fill="y",side="right",anchor="w")
+        self.datatree.configure(yscrollcommand=self.datatreescroll.set,show="tree")
+        self.datatreescroll.configure(command=self.datatree.yview)
         self.keylist.bind("<<ListboxSelect>>",self.keylist_select)
-        self.datalist.bind("<<ListboxSelect>>",self.datalist_select)
+        self.datatree.bind("<<TreeviewSelect>>",self.datatree_select)
+        self.notectlist.bind("<<ListboxSelect>>",self.notectlist_select)
         self.update_ui()
 
 
